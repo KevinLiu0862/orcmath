80549bdd6276f603431e75c4c296e5eeb29542a8
BT_1N3_11 <BT_1N3_11@BT_1N3_11.bktech.local>
2018-01-08 15:27:49 -0500

    sd

---
+++ OrcMathGUI/src/myStuff/simon/ButtonKevin.java
@@ -0,0 +1,39 @@
+package myStuff.simon;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import guiTeacher.components.Action;
+import guiTeacher.components.Button;
+
+public class ButtonKevin extends Button implements ButtonInterfaceKevin{
+
+	public ButtonKevin(int x, int y, int w, int h, String text, Action action) {
+		super(x, y, w, h, "", null);
+
+		update();
+	}
+
+	@Override
+	public void setColor(Color color) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void highlight() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dim() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void drawButton(Graphics2D g, boolean hover) {
+		
+	}
+
+}
---
+++ OrcMathGUI/src/myStuff/simon/MoveKevin.java
@@ -0,0 +1,24 @@
+package myStuff.simon;
+
+import guiTeacher.components.Action;
+
+public class MoveKevin implements MoveInterfaceKevin{
+
+	public MoveKevin() {
+		super();
+		
+	}
+	
+	@Override
+	public Object get(int sequenceIndex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ButtonInterfaceKevin getAButton() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
